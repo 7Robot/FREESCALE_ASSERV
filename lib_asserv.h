1cc330fc6ff9b60b6eec58dfd61439f5e0027cf4
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2014-05-01 00:09:13 +0200

    modif de MotionState pour avoir plus d'info, signature de motion_step

--- lib_asserv.h
+++ lib_asserv.h
@@ -12,4 +12,8 @@ void motion_speed_dir(float speed, float direction);
 // rouler à une certaine vitesse en restant à une certaine position sur la piste
 void motion_speed_pos(float speed, float pos);
 
+
+// renvoie la commande de vitesse et de direction : à appeler dans l'interruption
+void motion_step(int tics, float pos, float forecast_pos, float *cmd_speed, float *cmd_dir);
+
 #endif
--- private/motion.h
+++ private/motion.h
@@ -5,9 +5,12 @@
 
 // état de la voiture (position sur piste, vitesse, direction)
 typedef struct{
-   float pos;
-   float speed;
-   float dir;
+   float pos;     // position sur la ligne
+   float d_pos;   // dérivée de pos
+   float speed;   // vitesse de la voiture
+   float acc;     // accélération de la voiture
+   float dir;     // direction de la voiture
+   float d_dir;   // dérivée de dir
 } MotionState;
 
 /*################################   FUNCTIONS   #################################*/
@@ -20,5 +23,8 @@ void motion_speed_dir(float speed, float direction);
 
 // rouler à une certaine vitesse en restant à une certaine position sur la piste
 void motion_speed_pos(float speed, float pos);
+
+// renvoie la commande de vitesse et de direction : à appeler dans l'interruption
+void motion_step(int tics, float pos, float forecast_pos, float *cmd_speed, float *cmd_dir);
 
 #endif
