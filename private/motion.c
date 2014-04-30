b53b1f7e13303b6cbfee5a2d4fa8095b93b214ed
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2014-04-30 20:06:40 +0200

    utilisation de colorgcc dans le makefile, 2-3 autres petites modifs

--- private/motion.c
+++ private/motion.c
@@ -7,6 +7,14 @@ volatile MotionState motionState;
 
 /*################################   FUNCTIONS   #################################*/
 
+// se mettre en roue libre
+void motion_free(){
+}
+
 // rouler à une certaine vitesse avec une certaine direction
-void motion_speed(float speed, float direction){
+void motion_speed_dir(float speed, float direction){
+}
+
+// rouler à une certaine vitesse en restant à une certaine position sur la piste
+void motion_speed_pos(float speed, float pos){
 }
